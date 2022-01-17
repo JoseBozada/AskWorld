5fa082ba436b8a539da0df73e236212dc520a879
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-01-17 16:23:46 +0100

    Create salir.php
    Creado carpeta sesión y archivo salir.php.

---
+++ Codigo/AskWorld/zonaAdmin/sesion/salir.php
@@ -0,0 +1,10 @@
+<?php
+    //Traemos la sesión anterior
+    session_start();
+
+    //Destruimos la sesión para que el usuario tenga que loguearse de nuevo
+    session_destroy();
+
+    echo "<script>alert('Desconectándose.')</script>";
+    echo "<script>window.open('../login.php','_self')</script>";
+?>
