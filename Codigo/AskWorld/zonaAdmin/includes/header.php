234bf371036124814e0f74fad4bd939f4240b1b2
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-01-19 16:28:01 +0100

    Carpeta IMG, includes y DAO
    Añado los archivos y carpetas indicadas.

---
+++ Codigo/AskWorld/zonaAdmin/includes/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Zona Administrador</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="../../librerias/datatable/dataTables.bootstrap4.min.css">
+    
+</head>
+<body>
+    <!-- Barra de Navegación -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div class="container">
+            <a class="navbar-brand" href="../../index.php">
+                <img src="../../img/logo.png" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../index.php"> Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../administrar/usuarios/usuarios.php"> Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../administrar/publicaciones/publicaciones.php"> Publicaciones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../administrar/comentarios/comentarios.php"> Comentarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../administrar/categorias/categorias.php"> Categorías</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../sesion/perfil.php"> Mi Cuenta</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../sesion/salir.php" style="color: red"><span class="fas fa-power-off"></span> Salir</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
