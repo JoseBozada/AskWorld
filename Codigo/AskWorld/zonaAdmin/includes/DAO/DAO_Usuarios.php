48f5811aa694f3e4110287a5a5d6b5035fcbc7f8
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-01-22 12:59:17 +0100

    Borrado directorio DAO
    Se elimina para subir los DAO actualizados

--- Codigo/AskWorld/zonaAdmin/includes/DAO/DAO_Usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    function mostrarUsuarios($conexion){
-        $consulta = "SELECT * from `usuarios`";
-        $resultado = mysqli_query($conexion, $consulta);
-        return $resultado;
-    }
-
-?>
