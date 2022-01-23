70478b4e3e5144b0b524ef6d7bd0f5e0689d6146
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-01-23 16:12:21 +0100

    Update recuperarPassword.php

--- Codigo/AskWorld/zonaAdmin/sesion/recuperacion/recuperarPassword.php
+++ Codigo/AskWorld/zonaAdmin/sesion/recuperacion/recuperarPassword.php
@@ -16,7 +16,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Recuperar Contraseña</title>
-	<link rel="stylesheet" href="css/login.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Bootstrap CSS -->
