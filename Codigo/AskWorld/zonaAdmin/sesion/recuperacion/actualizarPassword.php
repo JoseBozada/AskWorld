dcc1923a3529cb5114b86b6773b1b7be113c8556
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-01-23 16:12:36 +0100

    Update actualizarPassword.php

--- Codigo/AskWorld/zonaAdmin/sesion/recuperacion/actualizarPassword.php
+++ Codigo/AskWorld/zonaAdmin/sesion/recuperacion/actualizarPassword.php
@@ -11,7 +11,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Actualizar Contraseña</title>
-	<link rel="stylesheet" href="css/login.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
