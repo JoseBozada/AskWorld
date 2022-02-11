41b4eb912cf317bea9acb44982c0a72911d695fd
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-11 15:15:46 +0100

    Update DAO_Usuarios.php
    Añadido nuevas funciones

--- Codigo/AskWorld/zonaAdmin/includes/DAO/DAO_Usuarios.php
+++ Codigo/AskWorld/zonaAdmin/includes/DAO/DAO_Usuarios.php
@@ -1,5 +1,17 @@
 <?php
 
+    function consultaUsuario($conexion, $usuario, $password){
+        $consulta = "SELECT * FROM `usuarios` WHERE `Usuario` = '$usuario' AND `Contrasena` = '$password'";
+        $resultado = mysqli_query($conexion, $consulta);
+        return $resultado;
+    }
+
+    function consultaCorreoUsuario($conexion, $email){
+        $consulta = "SELECT * FROM `usuarios` WHERE `Email` = '$email'";
+        $resultado = mysqli_query($conexion, $consulta);
+        return $resultado;
+    }
+
     function mostrarUsuarios($conexion){
         $consulta = "SELECT * FROM `usuarios` WHERE Rol = 'Usuario'";
         $resultado = mysqli_query($conexion, $consulta);
@@ -42,8 +54,14 @@
         return $resultado;
     }
 
-    function ActualizarUsuario($conexion, $usuario, $password, $nombre, $apellido1, $apellido2, $email, $imagen, $fecha, $direccion, $telefono, $provincia, $poblacion, $idUsuario){
-        $consulta = "UPDATE `usuarios` SET `Usuario` = '$usuario', `Contrasena` = '$password', `Nombre` = '$nombre', `Apellido1` = '$apellido1', `Apellido2` = '$apellido2', `Email` = '$email', `Imagen` = '$imagen', `FechaNacimiento` = '$fecha', `Direccion` = '$direccion', `Telefono` = '$telefono', `Provincia` = '$provincia', `Poblacion` = '$poblacion' WHERE (`idUsuario` = '$idUsuario')";
+    function ActualizarUsuario($conexion, $usuario, $nombre, $apellido1, $apellido2, $email, $imagen, $fecha, $direccion, $telefono, $provincia, $poblacion, $idUsuario){
+        $consulta = "UPDATE `usuarios` SET `Usuario` = '$usuario', `Nombre` = '$nombre', `Apellido1` = '$apellido1', `Apellido2` = '$apellido2', `Email` = '$email', `Imagen` = '$imagen', `FechaNacimiento` = '$fecha', `Direccion` = '$direccion', `Telefono` = '$telefono', `Provincia` = '$provincia', `Poblacion` = '$poblacion' WHERE (`idUsuario` = '$idUsuario')";
+        $resultado = mysqli_query($conexion, $consulta);
+        return $resultado;
+    }
+
+    function ActualizarPasswordUsuario($conexion, $password, $token){
+        $consulta = "UPDATE `usuarios` SET `Contrasena` = '$password' WHERE `Token` = '$token'";
         $resultado = mysqli_query($conexion, $consulta);
         return $resultado;
     }
@@ -52,5 +70,16 @@
         $consulta = "DELETE FROM `usuarios` WHERE `idUsuario` = '$idUsuario'";
         $resultado = mysqli_query($conexion, $consulta);
         return $resultado;
+    }
+
+    function crearSesion($usuario){
+        //Queremos que el id de sesión sea su usuario
+        session_id($usuario['Usuario']);
+        //Creamos la conexion
+        session_start();
+        //Almacenamos en la sesión los datos del usuario
+        foreach($usuario as $indice=>$valor){
+            $_SESSION[$indice] = $valor;
+        }
     }
 ?>
