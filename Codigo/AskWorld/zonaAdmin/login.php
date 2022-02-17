1ba0baca5b39c117bea69b8e16a424946fd68f4a
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-17 16:20:59 +0100

    Update login.php
    Añadido validación y corregido ruta a index

--- Codigo/AskWorld/zonaAdmin/login.php
+++ Codigo/AskWorld/zonaAdmin/login.php
@@ -30,14 +30,14 @@ $codigo = "4e7afebcfbae000b22c7c85e5560f89a2a0280b4";
                             <span class="input-group-text" id="basic-addon1">
                                 <span><i class="fa fa-lock"></i></span>
                             </span>
-                            <input type="password" name="password" id="password" class="form-control" placeholder="*********" required>
+                            <input type="password" name="passwordC" id="passwordC" class="form-control" placeholder="*********">
                         </div>
                     </div>
                 </div>
                 <br>
                 <div class="form-group">
                     <button type="submit" name="codigo" class="btn btn-primary btn-block"> Mandar Código</button>
-					<button class="btn btn-primary btn-block"> <a style="color: white; text-decoration:none;" href="../index.php">Página Principal</a></button>
+					<button class="btn btn-primary btn-block"> <a style="color: white; text-decoration:none;" href="../index.php?categoria=0">Página Principal</a></button>
                 </div>   
             </form>
         </article>
@@ -49,20 +49,20 @@ $codigo = "4e7afebcfbae000b22c7c85e5560f89a2a0280b4";
 if(isset($_POST['codigo'])){
 
 //Recogemos la contraseña introducida en el formulario
-$password = sha1($_POST["password"]);
+$password = sha1($_POST["passwordC"]);
 
 //Si la contraseña no coincide con el código echaremos al intruso. Si es correcto se mostrará el login del admin
 if ($password != $codigo) { 
 	echo "<script>alert('Código incorrecto, echando de la página.')</script>";
-	echo "<script>window.open('../index.php','_self')</script>";
+	echo "<script>window.open('../index.php?categoria=0','_self')</script>";
 	}else{
 ?>
 	<div class="text-center">
 		<div class="card-body">
-			<img src="img/Administrador.jpg" width="375" height="225" style="border-radius: 20px;">
+			<img src="../img/Administrador.jpg" width="375" height="225" style="border-radius: 20px;" class="img-thumbnail">
 			<article class="card-body mx-auto" style="max-width: 400px;">
 				<h4 class="card-title mt-3 text-center">Iniciar Sesión como Administrador</h4>
-				<form action="sesion/comprobarAdministrador.php" id="formLogin" method="post">
+				<form action="sesion/comprobarAdministrador.php" id="formLogin" method="post" class="needs-validation">
 					<!-- Usuario -->
 					<h5 style="text-align:left">Usuario</h5>
 					<div class="form-group input-group">
@@ -74,6 +74,7 @@ if ($password != $codigo) {
 									</svg>
 								</span>
 								<input type="text" name="usuario" id="usuario" class="form-control" placeholder="Usuario" required autofocus>
+								<div class="invalid-feedback">El usuario no es válido.</div>
 							</div>
 						</div>	
 					</div>
@@ -91,6 +92,7 @@ if ($password != $codigo) {
 									</svg>
 								</span>
 								<input type="password" name="password" id="password" class="form-control" placeholder="********" required>
+								<div class="invalid-feedback">La contraseña no es válida.</div>
 							</div>
 						</div>	
 					</div>
@@ -113,6 +115,9 @@ if ($password != $codigo) {
 
 	<!-- Bootstrap Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+	<!-- Validación -->
+	<script src="../js/validacionLogin.js"></script>
 <?php 
 	}
 } 
