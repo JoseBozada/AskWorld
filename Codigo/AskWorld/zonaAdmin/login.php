8977a89648bfd6a27f2de87b0f0011c28d942f55
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-17 16:23:11 +0100

    Update login.php
    Corregido tabulaciones

--- Codigo/AskWorld/zonaAdmin/login.php
+++ Codigo/AskWorld/zonaAdmin/login.php
@@ -7,15 +7,14 @@ $codigo = "4e7afebcfbae000b22c7c85e5560f89a2a0280b4";
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Zona de Administrador</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<meta charset="UTF-8">
+<title>Zona de Administrador</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <br>
@@ -36,8 +35,8 @@ $codigo = "4e7afebcfbae000b22c7c85e5560f89a2a0280b4";
                 </div>
                 <br>
                 <div class="form-group">
-                    <button type="submit" name="codigo" class="btn btn-primary btn-block"> Mandar Código</button>
-					<button class="btn btn-primary btn-block"> <a style="color: white; text-decoration:none;" href="../index.php?categoria=0">Página Principal</a></button>
+                	<button type="submit" name="codigo" class="btn btn-primary btn-block"> Mandar Código</button>
+			<button class="btn btn-primary btn-block"> <a style="color: white; text-decoration:none;" href="../index.php?categoria=0">Página Principal</a></button>
                 </div>   
             </form>
         </article>
