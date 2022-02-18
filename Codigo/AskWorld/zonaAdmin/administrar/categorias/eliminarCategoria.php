9ca94988a1f0823aeda3f582acb2b12e16a6e7e4
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-18 15:09:20 +0100

    Update eliminarCategoria.php
    Añadido que se borren las imágenes de las publicaciones que pertenecen a una categoría.

--- Codigo/AskWorld/zonaAdmin/administrar/categorias/eliminarCategoria.php
+++ Codigo/AskWorld/zonaAdmin/administrar/categorias/eliminarCategoria.php
@@ -1,28 +1,38 @@
 <?php
+
 	//Incluimos el conector a la Base de datos 
-    include "../../includes/Database.php";
+    	include "../../includes/Database.php";
 
-    //Incluimos los ficheros donde están las funciones
-    require "../../includes/DAO/DAO_Categorias.php";
-    require "../../includes/DAO/DAO_Publicaciones.php";
+	//Incluimos los ficheros donde están las funciones
+	require "../../includes/DAO/DAO_Categorias.php";
+	require "../../includes/DAO/DAO_Publicaciones.php";
 
-    //Recogemos el ID recibido por URL
-    $idURL = $_GET["id"];
+	//Recogemos el ID recibido por URL
+	$idURL = $_GET["id"];
 
-    //Eliminamos la categoría
-    $consulta = eliminarCategoria($conexion, $idURL);
+	//Buscamos las imágenes de las publicaciones y procedemos a eliminarlas.
+	$get_publicaciones = mostrarPublicacionesPorCategoria($conexion, $idURL);
+
+	while($row_publicaciones = mysqli_fetch_assoc($get_publicaciones)) {
+
+		$imagenPublicacion = $row_publicaciones['ImagenPublicacion'];
+
+		unlink($imagenPublicacion);
+	    }
+
+	//Eliminamos la categoría
+	$consulta = eliminarCategoria($conexion, $idURL);
 
 	if($consulta){
 
-        echo "<script>alert('La categoría ha sido eliminada correctamente.')</script>";
+		echo "<script>alert('La categoría ha sido eliminada correctamente.')</script>";
 
 		echo "<script>window.open('categorias.php','_self')</script>";
 
 	} else {
 
 		echo "<script>alert('La categoría no se ha podido eliminar.')</script>";
-            
-        echo "<script>window.open('categorias.php','_self')</script>";
-	}
 
+		echo "<script>window.open('categorias.php','_self')</script>";
+	}
 ?>
