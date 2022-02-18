14f7c9194bdd17502483169d6cc195be03754901
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-18 15:06:24 +0100

    Update tablaPublicaciones.php
    Corregido líneas.

--- Codigo/AskWorld/zonaAdmin/administrar/publicaciones/tablaPublicaciones.php
+++ Codigo/AskWorld/zonaAdmin/administrar/publicaciones/tablaPublicaciones.php
@@ -28,54 +28,69 @@
                 <tbody>
                     <?php
 
-                        //Contador
-						$i=0;
+			//Contador
+			$i=0;
 
-    					$consulta = mostrarPublicaciones($conexion);
+    			$consulta = mostrarPublicaciones($conexion);
 
-						//Recorreremos la tabla de las publicaciones  y mostraremos sus datos
-	                    while($mostrar = mysqli_fetch_assoc($consulta)) {
+			//Recorreremos la tabla de las publicaciones y mostraremos sus datos
+	                while($mostrar = mysqli_fetch_assoc($consulta)) {
 
-                        //Consulta de categorias: Necesitamos el nombre para mostrarlo en la tabla.
-                        $idCategorias = $mostrar['idCategoria'];
+				//Consulta de categorias: Necesitamos el nombre para mostrarlo en la tabla.
+				$idCategorias = $mostrar['idCategoria'];
 
-                        $get_categorias = mostrarCategoriasPorID($conexion, $idCategorias);
+				$get_categorias = mostrarCategoriasPorID($conexion, $idCategorias);
 
-                        $row_categorias = mysqli_fetch_assoc($get_categorias);
+				$row_categorias = mysqli_fetch_assoc($get_categorias);
 
-                        $nombreCategoria = $row_categorias['NombreCategoria'];
+				$nombreCategoria = $row_categorias['NombreCategoria'];
 
-                        //Consulta de usuarios: Necesitamos el nombre para mostrarlo en la tabla.
-                        $idUsuario = $mostrar['idUsuario'];
+				//Consulta de usuarios: Necesitamos el nombre para mostrarlo en la tabla.
+				$idUsuario = $mostrar['idUsuario'];
 
-                        $get_usuarios = mostrarUsuariosPorID($conexion, $idUsuario);
+				$get_usuarios = mostrarUsuariosPorID($conexion, $idUsuario);
 
-                        $row_usuarios = mysqli_fetch_assoc($get_usuarios);
+				$row_usuarios = mysqli_fetch_assoc($get_usuarios);
 
-                        $nombreUsuario = $row_usuarios['Usuario'];
+				$nombreUsuario = $row_usuarios['Usuario'];
 
-	                    //Guardamos los datos que necesitamos de la publicación
-	                    $id_publicacion = $mostrar['idPublicacion'];
+				//Guardamos los datos que necesitamos de la publicación
+				$id_publicacion = $mostrar['idPublicacion'];
 
-                        $imagenPublicacion = $mostrar['ImagenPublicacion'];
+				$imagen_Publicacion = $mostrar['ImagenPublicacion'];
 
-                        //Aumentamos el contador
-						$i++;
+				//Eliminamos la ruta de la imagen
+				$imagenPublicacion = str_replace("../", "", $imagen_Publicacion);
+
+				$nombrePublicacion = $mostrar['NombrePublicacion'];
+
+				$descripcionPublicacion = $mostrar['DescripcionPublicacion'];
+
+				$fechaPublicacion = $mostrar['FechaPublicacion'];
+
+				//Aumentamos el contador
+				$i++;
                 	?>
                     <tr style="text-align: center;">
                         <td><strong><?php echo $i ?></strong></td>
-						<td><strong><?php echo $nombreCategoria ?></strong></td>
+			<td><strong><?php echo $nombreCategoria ?></strong></td>
                         <td><strong><?php echo $nombreUsuario ?></strong></td>
-                        <td><strong><?php echo $mostrar['NombrePublicacion'] ?></strong></td>
-                        <td><strong><?php echo $mostrar['DescripcionPublicacion'] ?></strong></td>
-                        <td><strong><?php echo $mostrar['FechaPublicacion'] ?></strong></td>
-                        <td> <img src="<?php echo $imagenPublicacion; ?>" width="100" height="80"></td>
-                        <td style="text-align: center;">
+                        <td>
+                            <strong>
+                                <a class="text-dark" style="text-decoration:none" href="../../../detallesPublicacion.php?publicacion=<?php echo $id_publicacion; ?>" target="_blank">
+                                    <?php echo $nombrePublicacion ?>
+                                </a>
+                            </strong>
+                        </td>
+                        <td><strong><?php echo $descripcionPublicacion ?></strong></td>
+                        <td><strong><?php echo $fechaPublicacion ?></strong></td>
+                        <td><img src="../../../<?php echo $imagenPublicacion; ?>" width="100" height="80"></td>
+                        <td>
                             <a class="btn btn-warning btn-sm" href="editarPublicacion.php?id=<?php echo $id_publicacion; ?>">
                                 <span class="fas fa-edit"></span>
                             </a>
                         </td>
-                        <td style="text-align: center;"> 
+                        <td> 
                             <a class="btn btn-danger btn-sm" onClick="return confirm('¿Estas seguro de que quieres eliminar esta publicación?');" href="eliminarPublicacion.php?id=<?php echo $id_publicacion; ?>">
                                 <span class="fas fa-trash-alt"></span>
                             </a> 
