9e2387dfe1b48b14b26e55b15d91fd89ba4cb364
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-18 15:35:57 +0100

    Update tablaUsuarios.php
    Corregido líneas.

--- Codigo/AskWorld/zonaAdmin/administrar/usuarios/tablaUsuarios.php
+++ Codigo/AskWorld/zonaAdmin/administrar/usuarios/tablaUsuarios.php
@@ -32,24 +32,25 @@
 				</thead>
 				<tbody>
 					<?php
-					    
 						//Contador
 						$i=0;
 
-    					$consulta = mostrarUsuarios($conexion);
+    						$consulta = mostrarUsuarios($conexion);
 						
 						//Recorreremos la tabla de los usuarios y mostraremos sus datos
-	                    while($mostrar = mysqli_fetch_array($consulta)) {
+						while($mostrar = mysqli_fetch_array($consulta)) {
+							
+							//Guardamos los datos que necesitamos
+							$id_usuario = $mostrar['idUsuario'];
 
-	                    //Guardamos los datos que necesitamos
-	                    $id_usuario = $mostrar['idUsuario'];
-	                    
-						$imagen = $mostrar['Imagen'];	
+							$imagenUsuario = $mostrar['Imagen'];
 
-						//Aumentamos el contador
-						$i++;
+							//Eliminamos la ruta de la imagen para mostrarla
+							$imagen = str_replace("../", "", $imagenUsuario);
 
-                	?>
+							//Aumentamos el contador
+							$i++;
+                			?>
 					<tr style="text-align: center;">
 						<td><strong><?php echo $i ?></strong></td>
 						<td><strong><?php echo $mostrar['Usuario']; ?></strong></td>
@@ -57,27 +58,27 @@
 						<td><strong><?php echo $mostrar['Apellido1']; ?></strong></td>
 						<td><strong><?php echo $mostrar['Apellido2']; ?></strong></td>
 						<td><strong><?php echo $mostrar['Email']; ?></strong></td>
-						<td> <img src="<?php echo $imagen; ?>" width="100" height="80"></td>
+						<td> <img src="../../../<?php echo $imagen; ?>" width="100" height="100"></td>
 						<td><strong><?php echo $mostrar['FechaNacimiento']; ?></strong></td>
 						<td><strong><?php echo $mostrar['Direccion']; ?></strong></td>
 						<td><strong><?php echo $mostrar['Telefono']; ?></strong></td>
 						<td><strong><?php echo $mostrar['Provincia']; ?></strong></td>
 						<td><strong><?php echo $mostrar['Poblacion']; ?></strong></td>
 						<td><strong><?php echo $mostrar['DNI']; ?></strong></td>
-						<td style="text-align: center;">
-                            <a class="btn btn-warning btn-sm" href="editarUsuario.php?id=<?php echo $id_usuario; ?>">
-                                <span class="fas fa-edit"></span>
-                            </a>
-                        </td>
-                        <td style="text-align: center;"> 
-                            <a class="btn btn-danger btn-sm" onClick="return confirm('¿Estas seguro de que quieres eliminar este Usuario?');" href="eliminarUsuario.php?id=<?php echo $id_usuario; ?>">
-                                <span class="fas fa-trash-alt"></span>
-                            </a> 
-                        </td>
+						<td>
+						    <a class="btn btn-warning btn-sm" href="editarUsuario.php?id=<?php echo $id_usuario; ?>">
+							<span class="fas fa-edit"></span>
+						    </a>
+						</td>
+						<td> 
+						    <a class="btn btn-danger btn-sm" onClick="return confirm('¿Estas seguro de que quieres eliminar este usuario?');" href="eliminarUsuario.php?id=<?php echo $id_usuario; ?>">
+							<span class="fas fa-trash-alt"></span>
+						    </a> 
+						</td>
 					</tr>
 					<?php
-                		}
-                	?>
+						}
+					?>
 				</tbody>
 			</table>
 		</div>
