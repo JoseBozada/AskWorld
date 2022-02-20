23cd5ea166eabab5c50554a791c88b67bd3cc449
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-20 15:57:06 +0100

    Update tablaComentarios.php
    Corregido líneas.

--- Codigo/AskWorld/zonaUsuario/comentarios/tablaComentarios.php
+++ Codigo/AskWorld/zonaUsuario/comentarios/tablaComentarios.php
@@ -1,11 +1,11 @@
 <?php
-    //Incluimos el conector a la Base de datos e iniciamos la sesión
-    include "../../zonaAdmin/includes/Database.php";
-    session_start();
-    
-    //Incluimos los ficheros donde están las funciones
-    include "../../zonaAdmin/includes/DAO/DAO_Publicaciones.php";
-    include "../../zonaAdmin/includes/DAO/DAO_Comentarios.php";
+	//Incluimos el conector a la Base de datos e iniciamos la sesión
+	include "../../zonaAdmin/includes/Database.php";
+	session_start();
+
+	//Incluimos los ficheros donde están las funciones
+	include "../../zonaAdmin/includes/DAO/DAO_Publicaciones.php";
+	include "../../zonaAdmin/includes/DAO/DAO_Comentarios.php";
 ?>
 
 <div class="row">
@@ -16,7 +16,7 @@
                     <tr style="text-align: center; border: black 1px solid; background: orange">
                         <td>#</td>
                         <td>Publicación</td>
-                        <td>Imagen Publicación</td>
+                        <td>Imagen</td>
                         <td>Comentario</td>
                         <td>Fecha</td>
                         <td>Editar</td>
@@ -26,34 +26,34 @@
                 <tbody>
                     <?php
                         //Contador
-						$i=0;
+			$i=0;
 
                         $idUsuario = $_SESSION['idUsuario'];
                         
-    					$consulta = mostrarComentariosPorUsuario($conexion, $idUsuario);
+    			$consulta = mostrarComentariosPorUsuario($conexion, $idUsuario);
 
-						//Recorreremos la tabla de los comentarios y mostraremos sus datos
-	                    while($mostrar = mysqli_fetch_assoc($consulta)) {
+			//Recorreremos la tabla de los comentarios y mostraremos sus datos
+	                while($mostrar = mysqli_fetch_assoc($consulta)) {
+				
+				//Consulta de publicaciones: Necesitamos el nombre y la imagen para mostrarlo en la tabla.
+				$idPublicacion = $mostrar['idPublicacion'];
 
-                        //Consulta de publicaciones: Necesitamos el nombre y la imagen para mostrarlo en la tabla.
-                        $idPublicacion = $mostrar['idPublicacion'];
+				$get_Publicaciones = mostrarPublicacionesPorID($conexion, $idPublicacion);
 
-                        $get_Publicaciones = mostrarPublicacionesPorID($conexion, $idPublicacion);
+				$row_Publicacion = mysqli_fetch_assoc($get_Publicaciones);
 
-                        $row_Publicacion = mysqli_fetch_assoc($get_Publicaciones);
+				$nombrePublicacion = $row_Publicacion['NombrePublicacion'];
 
-                        $nombrePublicacion = $row_Publicacion['NombrePublicacion'];
+				$imgPublicacion = $row_Publicacion['ImagenPublicacion'];
 
-                        $imgPublicacion = $row_Publicacion['ImagenPublicacion'];
+				//Eliminamos la ruta de la imagen
+				$imagenPublicacion = str_replace("../", "", $imgPublicacion);
 
-                        //Eliminamos la ruta de la imagen
-                        $imagenPublicacion = str_replace("../", "", $imgPublicacion);
-                        
-                        //Guardamos el ID del comentario en una variable
-	                    $id_comentario = $mostrar['idComentario'];
+				//Guardamos el ID del comentario en una variable
+				$id_comentario = $mostrar['idComentario'];
 
-                        //Aumentamos el contador
-						$i++;
+				//Aumentamos el contador
+				$i++;
                 	   ?>
                     <tr style="text-align: center;">
                         <td><strong><?php echo $i ?></strong></td>
@@ -68,15 +68,15 @@
                         <td><strong><?php echo $mostrar['Comentario'] ?></strong></td>
                         <td><strong><?php echo $mostrar['Fecha'] ?></strong></td>
                         <td>
-							<a class="btn btn-warning btn-sm" href="editarComentario.php?id=<?php echo $id_comentario; ?>">
-								<span class="fas fa-edit"></span>
-							</a>
-						</td>
-						<td> 
-							<a class="btn btn-danger btn-sm" onClick="return confirm('¿Estas seguro de que quieres eliminar este Comentario?');" href="eliminarComentario.php?id=<?php echo $id_comentario; ?>">
-								<span class="fas fa-trash-alt"></span>
-							</a> 
-						</td>
+				<a class="btn btn-warning btn-sm" href="editarComentario.php?id=<?php echo $id_comentario; ?>">
+					<span class="fas fa-edit"></span>
+				</a>
+			</td>
+			<td> 
+				<a class="btn btn-danger btn-sm" onClick="return confirm('¿Estas seguro de que quieres eliminar este Comentario?');" href="eliminarComentario.php?id=<?php echo $id_comentario; ?>">
+					<span class="fas fa-trash-alt"></span>
+				</a> 
+			</td>
                     </tr>
                     <?php
                 		}
