7fc652ca406f18a396bc2aadbf9dbae4c8bb977d
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-02-28 15:44:22 +0100

    Update datos.sql
    Añadido creación de las tablas

--- DataBase/datos.sql
+++ DataBase/datos.sql
@@ -18,6 +18,21 @@ USE `askworld`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `NombreCategoria` varchar(20) NOT NULL,
+  PRIMARY KEY (`idCategoria`),
+  UNIQUE KEY `NombreCategoria_UNIQUE` (`NombreCategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `categoria`
 --
 
@@ -26,6 +41,28 @@ LOCK TABLES `categoria` WRITE;
 INSERT INTO `categoria` VALUES (2,'Borrar'),(3,'Informática'),(1,'Películas');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `comentarios`
+--
+
+DROP TABLE IF EXISTS `comentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentarios` (
+  `idComentario` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `idPublicacion` int(11) NOT NULL,
+  `Comentario` text COLLATE utf8_bin NOT NULL,
+  `Fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idComentario`),
+  KEY `fk_Usuario_idx` (`idUsuario`),
+  KEY `fk_Usuario_idx1` (`idPublicacion`),
+  KEY `fk_Usuario_idx_Comentario` (`idUsuario`),
+  CONSTRAINT `fk_idPublicacion` FOREIGN KEY (`idPublicacion`) REFERENCES `publicaciones` (`idPublicacion`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `comentarios`
@@ -38,6 +75,29 @@ INSERT INTO `comentarios` VALUES (1,1,1,'Os recomiendo ver esta película si os 
 UNLOCK TABLES;
 
 --
+-- Table structure for table `publicaciones`
+--
+
+DROP TABLE IF EXISTS `publicaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publicaciones` (
+  `idPublicacion` int(11) NOT NULL AUTO_INCREMENT,
+  `idCategoria` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `NombrePublicacion` varchar(45) NOT NULL,
+  `DescripcionPublicacion` text NOT NULL,
+  `FechaPublicacion` date NOT NULL,
+  `ImagenPublicacion` varchar(255) NOT NULL,
+  PRIMARY KEY (`idPublicacion`),
+  KEY `fk_Categoria_idx` (`idCategoria`),
+  KEY `fk_Usuario_idx` (`idUsuario`),
+  CONSTRAINT `fk_Categoria` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_Usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `publicaciones`
 --
 
@@ -48,6 +108,40 @@ INSERT INTO `publicaciones` VALUES (1,1,1,'Terminator','En 2029, después de dev
 UNLOCK TABLES;
 
 --
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `Usuario` varchar(20) COLLATE utf8_bin NOT NULL,
+  `Contrasena` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Nombre` varchar(20) COLLATE utf8_bin NOT NULL,
+  `Apellido1` varchar(20) COLLATE utf8_bin NOT NULL,
+  `Apellido2` varchar(20) COLLATE utf8_bin NOT NULL,
+  `Email` varchar(80) COLLATE utf8_bin NOT NULL,
+  `Imagen` varchar(255) COLLATE utf8_bin NOT NULL,
+  `FechaNacimiento` date NOT NULL,
+  `Direccion` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Telefono` int(9) NOT NULL,
+  `Provincia` varchar(45) COLLATE utf8_bin NOT NULL,
+  `Poblacion` varchar(45) COLLATE utf8_bin NOT NULL,
+  `DNI` varchar(9) COLLATE utf8_bin NOT NULL,
+  `Token` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Rol` varchar(45) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `Usuario_UNIQUE` (`Usuario`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`),
+  UNIQUE KEY `Telefono_UNIQUE` (`Telefono`),
+  UNIQUE KEY `DNI_UNIQUE` (`DNI`),
+  UNIQUE KEY `Token_UNIQUE` (`Token`),
+  KEY `fk_Publicacion` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `usuarios`
 --
 
@@ -56,6 +150,26 @@ LOCK TABLES `usuarios` WRITE;
 INSERT INTO `usuarios` VALUES (1,'Jose20','f0482c1b407fb3109ee3dca919175bad47eeea60','Jose','Bozada','Montanés','josebozadamontanes@gmail.com','../../img/usuarios/Broly LSSJ 2.jpg','1998-03-04','PadreLerchundi',654024147,'Melilla','Melilla','45316056F','5a065813082c8ed960b84c9165ead3','Admin'),(2,'Test11','f0482c1b407fb3109ee3dca919175bad47eeea60','test','test','test','test@gmail.com','img/usuarios/test.png','1993-01-01','Calle General Barceló 41, Melilla',123456789,'Ceuta','Ceuta','12345678A','5a065813082232d960b84c9165ead3','Usuario'),(3,'Borrar22','f0482c1b407fb3109ee3dca919175bad47eeea60','Borrar','Borrar','Borrar','Borrar@gmail.com','img/usuarios/avatar.png','1990-03-11','Sin direccion',213456789,'Madrid','Madrid','21345678E','5a0sdsdg3082232d960b84c9165ead3','Usuario');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `valoracion`
+--
+
+DROP TABLE IF EXISTS `valoracion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `valoracion` (
+  `idValoracion` int(11) NOT NULL AUTO_INCREMENT,
+  `idPublicacion` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `valoracion` int(1) NOT NULL,
+  PRIMARY KEY (`idValoracion`),
+  KEY `fk_Publicacion_idx` (`idPublicacion`),
+  KEY `fk_Usuario_idx` (`idUsuario`),
+  CONSTRAINT `fk_Publicacion_idx` FOREIGN KEY (`idPublicacion`) REFERENCES `publicaciones` (`idPublicacion`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_Usuario_idx` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `valoracion`
@@ -76,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-25 15:43:52
+-- Dump completed on 2022-02-28 15:43:24
