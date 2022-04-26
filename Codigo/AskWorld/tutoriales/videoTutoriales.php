5d7cb47acc11681a9acca4e238b39dcb33581468
JoseBozada <55547053+JoseBozada@users.noreply.github.com>
2022-04-26 17:41:43 +0200

    Add files via upload

---
+++ Codigo/AskWorld/tutoriales/videoTutoriales.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Melilla En Red</title>
+</head>
+<body>
+    <?php include '../includes/headerSimp.php'; ?>
+    
+    <div class="container responsive">
+        <h3 class="text-center mt-3 text-uppercase">VideoTutoriales</h3>
+        <div class="row mt-3">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="text-center text-uppercase">Registrarse</h5>
+                    </div>
+                    <div class="responsive" align="center">
+                        <object data="http://www.youtube.com/v/Ahg6qcgoay4" type="application/x-shockwave-flash"><param name="src" value="http://www.youtube.com/v/Ahg6qcgoay4"/></object>
+                    </div>
+                    <div class="card-footer text-center">
+                        En este tutorial se enseñará a como registrarnos en la página.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="text-center text-uppercase">Registrarse</h5>
+                    </div>
+                    <div class="responsive" align="center">
+                        <object data="http://www.youtube.com/v/Ahg6qcgoay4" type="application/x-shockwave-flash"><param name="src" value="http://www.youtube.com/v/Ahg6qcgoay4"/></object>
+                    </div>
+                    <div class="card-footer text-center">
+                        En este tutorial se enseñará a como registrarnos en la página.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 mb-4">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="text-center text-uppercase">Registrarse</h5>
+                    </div>
+                    <div class="responsive" align="center">
+                        <object data="http://www.youtube.com/v/Ahg6qcgoay4" type="application/x-shockwave-flash"><param name="src" value="http://www.youtube.com/v/Ahg6qcgoay4"/></object>
+                    </div>
+                    <div class="card-footer text-center">
+                        En este tutorial se enseñará a como registrarnos en la página.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="text-center text-uppercase">Registrarse</h5>
+                    </div>
+                    <div class="responsive" align="center">
+                        <object data="http://www.youtube.com/v/Ahg6qcgoay4" type="application/x-shockwave-flash"><param name="src" value="http://www.youtube.com/v/Ahg6qcgoay4"/></object>
+                    </div>
+                    <div class="card-footer text-center">
+                        En este tutorial se enseñará a como registrarnos en la página.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include '../includes/footerSimp.php'; ?>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
